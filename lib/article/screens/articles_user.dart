f2fbba04ce1f669f91634cf01e1e8c5733a5e052
almerazka <almer.azka12@gmail.com>
2024-12-19 05:11:05 +0700

    Success everything about image

--- lib/article/screens/articles_user.dart
+++ lib/article/screens/articles_user.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:lohkan_app/article/models/article_entry.dart';
 import 'package:http/http.dart' as http; 
@@ -16,7 +17,7 @@ class _ArticleScreenState extends State<ArticleScreenUser> {
 
   // Fungsi untuk mengambil data dari API
   Future<List<ArticleEntry>> fetchArticles() async {
-    // final response = await http.get(Uri.parse('http://10.0.2.2:8000/article/json/'));
+    // final response = await http.get(Uri.parse('http://127.0.0.1:8000/article/json/'));
     final response = await http.get(Uri.parse('http://marla-marlena-lohkan.pbp.cs.ui.ac.id/article/json/'));
     if (response.statusCode == 200) {
       return articleEntryFromJson(response.body);
